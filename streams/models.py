05a0e2bdb99a8affad1ee50579714fbd4b84bf4d
Yatharth Goswami <yatharth@Asus-ROG.local>
2020-04-07 21:00:38 +0530

    #3 Add the streams module

--- streams/models.py
+++ streams/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-#from users.models import User
-#from posts.models import Posts
+from users.models import User
+from posts.models import Posts
 # Create your models here.
 
 class Stream(models.Model):
     title = models.CharField(max_length = 50)
     description = models.CharField(max_length = 1000)
-    #posts = models.ManyToManyField(Posts, blank = True)
-    #followed_by = models.ManyToManyField(User, blank = True)
+    posts = models.ManyToManyField(Posts, blank = True)
+    followed_by = models.ManyToManyField(User, blank = True)
     follower_number = models.IntegerField()
 
     def __str__(self):
