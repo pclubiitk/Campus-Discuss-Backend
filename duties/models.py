97d72fa169485ec53ce2fca4f8e7bdc24cd6c3e8
aashishpiitk <aashishp@iitk.ac.in>
2020-04-08 03:18:11 +0530

    #4 Make Duties model/table # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: #	modified:   duties/models.py #

--- duties/models.py
+++ duties/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Moderators(models.Model):
     moderator=models.ForeignKey('users.User')
+    
     streams=models.ManyToManyField('streams.Streams')
     #powers=models.ForeignKey('powers.Powers')
 
