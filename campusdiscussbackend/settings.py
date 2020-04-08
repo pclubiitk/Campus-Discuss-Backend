917193511c51f2f8ee492aa3a8a1fb3db55fe24b
SomTambe <tambesom@gmail.com>
2020-04-08 15:43:50 +0530

    Add post model

--- campusdiscussbackend/settings.py
+++ campusdiscussbackend/settings.py
@@ -99,7 +99,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-#from users.models import User
+from users.models import User
 
 
 class Post(models.Model):
 	post_text  = models.CharField(max_length = 5000)
 	post_title = models.CharField(max_length = 100)
 	pub_date= models.DateTimeField('date field')
-#	post_creator = models.ForeignKey(User , on_delete= models.CASCADE)
+	author = models.ForeignKey(User , on_delete= models.CASCADE)
