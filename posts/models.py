a047a961c32d4fdd9e7b3c002ae6dac1ec6243e5
shubh10 <shubhagrawal101220001@gmail.com>
2020-04-08 18:09:25 +0530

    Correct name of Post model in stream app

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
 	post_title = models.CharField(max_length = 100)
 	pub_date= models.DateTimeField('date field')
 	author = models.ForeignKey(User , on_delete= models.CASCADE)
+
+	def __str__(self):
+		return self.post_title
--- streams/models.py
+++ streams/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from users.models import User
-from posts.models import Posts
+from posts.models import Post
 # Create your models here.
 
 class Stream(models.Model):
     title = models.CharField(max_length = 50)
     description = models.CharField(max_length = 1000)
-    posts = models.ManyToManyField(Posts, blank = True)
+    posts = models.ManyToManyField(Post, blank = True)
     followed_by = models.ManyToManyField(User, blank = True)
 
     def __str__(self):
