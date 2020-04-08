8868c1c4bcfefdf0f7fb81121291cc088b5cf6ee
shubh10 <58140016+shubh10@users.noreply.github.com>
2020-04-08 23:35:49 +0530

    #6 Add many to many relations from streams to posts

--- posts/models.py
+++ posts/models.py
@@ -6,4 +6,7 @@ class Post(models.Model):
 	post_text  = models.CharField(max_length = 5000,null=True,blank=True)
 	post_title = models.CharField(max_length = 100,null=True,blank=True)
 	pub_date= models.DateTimeField('date field')
-	#author = models.ForeignKey(User , on_delete= models.CASCADE)
+	author = models.ForeignKey(User , on_delete= models.CASCADE)
+
+	def __str__(self):
+		return self.post_title
--- streams/models.py
+++ streams/models.py
@@ -1,17 +1,14 @@
 from django.db import models
-#from users.models import User
-#from posts.models import Posts
+from users.models import User
+from posts.models import Post
 # Create your models here.
 
 class Stream(models.Model):
     title = models.CharField(max_length = 50)
     description = models.CharField(max_length = 1000)
-    #posts = models.ManyToManyField(Posts, blank = True)
-    #followed_by = models.ManyToManyField(User, blank = True)
+
+    posts = models.ManyToManyField(Post, blank = True)
+    followed_by = models.ManyToManyField(User, blank = True)
 
     def __str__(self):
         return self.title
-
-
-
-
