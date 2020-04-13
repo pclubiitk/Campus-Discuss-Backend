898e2d1fce4fab0f01a498a01f054dfd38da2c43
Yatharth Goswami <yatharthgoswami15@gmail.com>
2020-04-13 23:27:20 +0530

    "#30 Make Login and Logout System"

---
+++ users/utils.py
@@ -0,0 +1,12 @@
+from .models import User
+
+def IsLoggedIn(request):
+    if request.session.has_key("username"):
+        try:
+            user = User.objects.get(username=request.session["username"])
+            return user
+
+        except:
+            return None
+    else:
+        return None
