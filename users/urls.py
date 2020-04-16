4637076caf06c96014b391b7e35bbf51c3ae11b6
SomTambe <tambesom@gmail.com>
2020-04-16 22:05:02 +0530

    Add API for confirmation mail

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
-from .views import LoginView, LogoutView
+from .views import LoginView, LogoutView, ActivationMailer
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name="auth-login"),
     path('auth/logout/', LogoutView.as_view(), name="auth-logout"),
+    path('verify/', ActivationMailer, name="activation-verify")
+    # path('verify/', csrf_exempt(ActivationMailer))
 ]
