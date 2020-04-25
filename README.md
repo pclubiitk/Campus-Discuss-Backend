bf670c12d67279cc370976f497c27a310dcf3df4
nike0503 <nikejain@iitk.ac.in>
2020-04-25 13:27:20 +0530

    #32 Follow Streams API

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Campus-Discuss-Backend
+## API Endpoints
+A description of all the API endpoints, their URL and request parameters
+#### FollowStreamAPI
+To follow another user.
+```
+url: /streams/follow/
+method: PUT
+parameters = {"title" : "<title of the stream to be followed>"}
+```
+
--- streams/views.py
+++ streams/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.utils import IsLoggedIn
+from django.contrib.auth import login, logout
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Stream
+from users.models import User
 
-# Create your views here.
+class FollowStreamView(APIView):
+    
+    def put(self,request):
+        user = IsLoggedIn(request)
+        if user is not None:
+            request.session["username"] = user.username
+            try:
+                stream = Stream.objects.get(title = request.data.get("title"))
+                if stream is not None:
+                    stream.followed_by.add(user)
+                    stream.save()
+                    user.save()
+                    return Response(status=status.HTTP_200_OK)
+                else:
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_401_UNAUTHORIZED) 
+
+
