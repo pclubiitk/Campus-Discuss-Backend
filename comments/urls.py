47549bc7e76ef287c2cfbafe707ac0280fbd1e92
Naman Gupta <32638824+namangup@users.noreply.github.com>
2020-07-19 04:38:56 +0530

    Fetch comments by post (#81)

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('create/', CreateComment.as_view(), name="create-comment"),
     path('delete/', DeleteComment.as_view(), name="delete-comment"),
+    path('view/<int:pk>/', ViewComments.as_view(), name="view-comments")
 ]
-
