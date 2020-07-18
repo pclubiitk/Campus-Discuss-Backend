0a96ee6930a3200c4f78bd14b64076fcf9860a3e
SomTambe <tambesom@gmail.com>
2020-07-18 21:06:17 +0530

    Rectified spelling mistake

--- tokens/models.py
+++ tokens/models.py
@@ -4,5 +4,5 @@ from users.models import User
 # Create your models here.
 
 class Tokens(models.Model):
-    token = model.CharField(primary_key=True)
-    user = model.ManyToManyField(User, related_name="token")
+    token = models.CharField(max_length=100, primary_key=True)
+    user = models.ManyToManyField(User, related_name="token")
