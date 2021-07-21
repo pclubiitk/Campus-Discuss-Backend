52c9632f1ce3fac31469e5d680a031c10c6b651f
BipplavTiwari <84016130+BipplavTiwari@users.noreply.github.com>
2021-07-21 16:08:21 +0530

    Update settings_email.py

--- campusdiscussbackend/settings_email.py
+++ campusdiscussbackend/settings_email.py
@@ -6,8 +6,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 25
-EMAIL_HOST_USER = 'dtiwary1980@gmail.com'
-EMAIL_HOST_PASSWORD = 'dusudusu'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_SUBJECT = {
         "Activation": "Activation mail for Campus Discuss",
